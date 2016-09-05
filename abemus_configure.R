fb063a58be12ae43f1e93d665920d699ae768049
Nicola <casiraghinicola@gmail.com>
2016-09-05 09:07:15 +0200

    config_update

--- abemus_configure.R
+++ abemus_configure.R
@@ -3,50 +3,41 @@
 # [ General parameters ]
 
 # Output directory
-outdir = "~/Desktop/temp_working_folder/"
+outdir = "/scratch/sharedCO/Casiraghi/N250_insilico/bgprob_0001_rfac_75"
 
 # abemus main directory [ git clone ]
-abemusdir = "/Users/nicolacasiraghi/Documents/abemus"
+abemusdir = "/elaborazioni/sharedCO/Home_casiraghi/Prog/abemus/"
 
 # Sample info file 
-filesif = "/Users/nicolacasiraghi/Desktop/temp_working_folder/N250_IPM_samples_with_germlines_info_file.tsv"
+filesif = "/scratch/sharedCO/Casiraghi/N250_insilico/bgprob_0001_rfac_75/sample_info_file.tsv"
 
 # bases in target regions [ previously obtained by using Rcript utility/PrepareTargetRegions.R ]
-targetbp = "/elaborazioni/sharedCO/PCF_Project/Analysis/SNVs/DUP/TargetPositions"
+targetbp = "/scratch/sharedCO/Casiraghi/N250_insilico/TargetPositions"
 
 # PacBam folder
-pacbamfolder = "/scratch/sharedCO/Casiraghi/Plasma_Project/DUP_q10/"
+pacbamfolder = "/CIBIO/sharedCO/PaCBAM/N250/data_N250_insilico_bgprob_0001_rfac_75_byChromosome/"
 
 # Number of threads
-mc.cores = 2
+mc.cores = 20 
 
 # abemus working steps
 # 1. Compute pbem in target positions exploiting germline samples only;
 # 2. Compute AF thresholds across coverage leveles by exploiting germline samples only;
 # 3. Call and annotate putative somatic SNVs in case samples;
 
-aws = c(1,2,3)
+aws = c(1)
 
-# [ 1. Estimation of allelic fraction thresold exploiting germline samples only ]
+# [ 1. Computatation of per-base error model ]
 
 # Bins of coverage into which divide allelic fractions
 coverage_binning = 50
 
-# [ 2. Computatation of per-base error model ]
-
-# list of putative somatic SNVs of interest and/or list of known somatic SNVs
-snvselection = "/elaborazioni/sharedCO/PCF_Project/Analysis/SNVs/DUP/Results/pmtab_F2_AllSamples.vcf"
-
-# Total number of random positions to build the berr reference random distribution
-bprandom = 1000
-
-# compute strand bias at SNP positions
-SNP = FALSE
+# [ 2. Estimation of allelic fraction thresold exploiting germline samples only ]
 
 # [ 3. Call SNVs in case samples ]
 
 # Control Data folder
-controls = "/elaborazioni/sharedCO/PCF_Project/Analysis/SNVs/DUP/Controls_V7_bq10_covbin50/"
+controls = ""
 
 ## Parameters to call somatic SNVs
 
