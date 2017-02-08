b65e6fb75f716e8b462875cfbfd61eee2e15fad7
Nicola <casiraghinicola@gmail.com>
2017-02-08 10:32:40 +0100

    split by chrom using tabix, do not consider clinvar as input

--- utility/dbSNPPER.R
+++ utility/dbSNPPER.R
@@ -1,102 +1,72 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs(trailingOnly = TRUE)
-if(length(args)!=4){
-  message("\nError!\nUSAGE: Invalid number of input parameters\n##### USAGE dbSNPPER.R <00-All.vcf> <clinvar_YYYYMMDD.vcf> <mytarget.bed> <path/to/outfolder>\n\n")
+if(length(args)!=3){
+  message("\nERROR:\t3 arguments required")
+  message("\nUSAGE:\tRscript dbSNPPER.R <00-All.vcf.gz> <BED> <outdir> \n")
+  cat(paste("\t[1] dbsnp\tThe dbsnp file 00-All.vcf.gz",
+            "\t[2] bed\t\tTarget regions in BED format",
+            "\t[3] outdir\tOutput folder",sep="\n"),"\n\n")  
   quit()
 }
 
-# 00-All.vcf | the dbSNP input file
-ALL=args[1]
-# clinvar_YYYYMMDD.vcf | the file reporting clinical rs ids
-CLINVAR=args[2]
-# BED target regions | BED file with regions covered by sequencing panel
-BED=args[3]
-# output folder
-outfolder=args[4]
+ALL = args[1]
+BED = args[2]
+outfolder = args[3]
 
 setwd(outfolder)
 timestart <- proc.time()
 
-message("# 1) Intersect dbSNP and BED of interest")
-out1 = gsub(basename(ALL),pattern = "\\.vcf",replacement = "_intersectbed.vcf")
-cmd = paste("bedtools intersect -a",ALL,"-b",BED,"-header -wa -u >",out1)
-system(cmd)
+message('DBSNP = ',ALL)
+message('BED = ',BED)
+message('OUTDIR = ',outfolder)
 
-message("# 2) Keep only SNVs")
-out2 = gsub(basename(ALL),pattern = "\\.vcf",replacement = "_intersectbed_snv_single.vcf")
-cmd = paste("head -n 500",out1,"| grep '^#' >",out2)
-system(cmd)
-
-tmp.snv = gsub(basename(ALL),pattern = "\\.vcf",replacement = "_tmp.vcf")
-cmd = paste("grep '\\;VC\\=SNV\\;'",out1,">",tmp.snv)
-system(cmd)
-
-message("# 3) Keep only SNVs with a single alternative allele")
-tmp.snv.single = gsub(basename(ALL),pattern = "\\.vcf",replacement = "_tmp_single.vcf")
-# GetSingleALT = "/home/nicola.casiraghi/Scripts/dbSNP_adjust/get_unique_alt.py"
-# cmd = paste("less",tmp.snv,"|",GetSingleALT,">",tmp.snv.single)
-cmd = paste("awk '$5 !~ /,/{print($0)}'",tmp.snv,">",tmp.snv.single)
-system(cmd)
-
-cmd = paste("cat",tmp.snv.single,">>",out2)
-system(cmd)
-
-cmdrm = paste("rm",tmp.snv,tmp.snv.single)
-system(cmdrm)
-
-if(FALSE){
-message("# 4) Filtering CLINVAR")
-clinvar.snv = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_fCLNSIG.vcf")
-cmd = paste("head -n 500",CLINVAR,"| grep '^#' >",clinvar.snv)
-system(cmd)
-
-tmp.snv = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_tmp.vcf")
-cmd = paste("grep '\\;VC\\=SNV\\;'",CLINVAR,">",tmp.snv)
-system(cmd)
-
-tmp.snv.single = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_tmp_single.vcf")
-cmd = paste("less",tmp.snv,"|",GetSingleALT,">",tmp.snv.single)
-system(cmd)
-
-tmp.snv.single.clnsig = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_tmp_single_clnsig.vcf")
-filterCLNSIG = "/home/nicola.casiraghi/Scripts/dbSNP_adjust/filter_clinvar_by_CLNSIG.py"
-cmd = paste("less",tmp.snv.single,"|",filterCLNSIG,">",tmp.snv.single.clnsig)
-system(cmd)
-
-cmd = paste("cat",tmp.snv.single.clnsig,">>",clinvar.snv)
-system(cmd)
-
-cmdrm = paste("rm",tmp.snv,tmp.snv.single,tmp.snv.single.clnsig)
-system(cmdrm)
-
-message("# 5) Extract CLINVAR somatic")
-clinvar.somatic = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_fCLNSIG_somatic.vcf")
-cmd = paste("head -n 500",CLINVAR,"| grep '^#' >",clinvar.somatic)
-system(cmd)
-
-tmp.somatic1 = gsub(basename(CLINVAR),pattern = "\\.vcf",replacement = "_tmpsomatic.vcf")
-cmd = paste("grep '\\;CLNORIGIN\\=2\\;'",clinvar.snv,">",tmp.somatic1)
-system(cmd)
-
-cmd = paste("cat",tmp.somatic1,">>",clinvar.somatic)
-system(cmd)
-
-cmdrm = paste("rm",tmp.somatic1)
-system(cmdrm)
-
-message("# 6) Get dbSNP with and without clinvar SNPs")
-nosomatic = gsub(out2,pattern = "\\.vcf",replacement = "_clinvarsomatic_excluded.vcf")
-cmd = paste("bedtools intersect -a",out2,"-b",clinvar.somatic,"-wa -v -header >",nosomatic)
-system(cmd)
-
-message("# 7) Split dbSNPs by chromosome")
-vcf2chrom = "Rscript /home/nicola.casiraghi/Scripts/dbSNP_adjust/VCFbyChromosome.R"
-bychr = file.path(outfolder,"ByChromosome")
-dir.create(bychr)
-cmd = paste(vcf2chrom,nosomatic,bychr)
-system(cmd)
+# check if 00-All.vcf.gz.tbi exists and create it if missing
+tbi = paste0(ALL,'.tbi')
+if(!file.exists(tbi)){
+  message("Creating tbi file for ",ALL)
+  cmd = paste('tabix -p vcf',ALL)
+  system(cmd)
 }
+
+b = read.delim(file = BED,as.is = T,header = F,stringsAsFactors = F)
+chromosomes = unique(sort(b[,1]))
+chromosomes = gsub(chromosomes,pattern = 'chr',replacement = '')
+
+bychromfolder = file.path(outfolder,paste0(basename(ALL),'.bychrom'))
+if(!dir.exists(bychromfolder)){
+  dir.create(bychromfolder)
+}
+setwd(bychromfolder)
+
+for(chr in chromosomes){
+  message('chrom:\t',chr)
+  outname = paste0('dbsnp_chr',chr,'.vcf')
+  cmd = paste('tabix',ALL,chr,'>',outname)
+  system(cmd)
+  # Intersect chrom dbsnp and bed
+  out1 = gsub(basename(outname),pattern = "\\.vcf",replacement = ".intersectbed.vcf")
+  cmd = paste("bedtools intersect -a",outname,"-b",BED,"-header -wa -u >",out1)
+  system(cmd)
+  # Keep only SNVs
+  out2 = gsub(basename(out1),pattern = "\\.vcf",replacement = ".snv.vcf")
+  cmd = paste("grep '\\;VC\\=SNV\\;'",out1,">",out2)
+  system(cmd)
+  # Keep only SNVs with a single alternative allele
+  out3 = gsub(basename(out2),pattern = "\\.vcf",replacement = ".single.vcf")
+  cmd = paste("awk '$5 !~ /,/{print($0)}'",out2,">",out3)
+  system(cmd)
+  # remove intersteps
+  cmd = paste('rm',outname,out1,out2)
+  system(cmd)
+}
+
+# Merge chrom vcfs into a single one 
+setwd(outfolder)
+vcfs = list.files(path = bychromfolder,pattern = '.intersectbed.snv.single.vcf$',full.names = T)
+outfinal = gsub(basename(ALL),pattern = '.vcf.gz',replacement = '.intersectbed.snv.single.vcf')
+cmd = paste('cat',paste(vcfs,collapse = ' '),'>',outfinal)
+system(cmd)
 
 proc.time()-timestart
 
